9f1af0c60617db1822e9eef32dc1a15b0e271a2e
deresmos <deresmos@gmail.com>
2018-01-25 21:53:15 +0900

    Add: gedit actions

--- rplugin/python3/denite/kind/gitdiffbranch.py
+++ rplugin/python3/denite/kind/gitdiffbranch.py
@@ -23,3 +23,17 @@ class Kind(Kind):
 
     def action_tabvdiff(self, context):
         self._run_gvdiff(context, self.action_tabopen)
+
+    def _run_gedit(self, context, func):
+        for target in context['targets']:
+            new_context = copy(context)
+            new_context['targets'] = [target]
+
+            func(new_context)
+            self.vim.command('Gedit {}:%'.format(target['base_revision']))
+
+    def action_gedit(self, context):
+        self._run_gedit(context, self.action_open)
+
+    def action_tabgedit(self, context):
+        self._run_gedit(context, self.action_tabopen)

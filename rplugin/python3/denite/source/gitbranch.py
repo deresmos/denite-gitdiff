cb5228d4a6a3dddb6238dda8fe867f6def2526ef
deresmos <deresmos@gmail.com>
2018-04-11 00:01:45 +0900

    Add gitbranch source

---
+++ rplugin/python3/denite/source/gitbranch.py
@@ -0,0 +1,33 @@
+import os
+
+from .gitdiffbranch import GitBase
+
+
+class Source(GitBase):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'gitbranch'
+        self.kind = 'gitbranch'
+
+    def on_init(self, context):
+        super().on_init(context)
+        cmd = self._cmd
+        cmd += [
+            'for-each-ref',
+            '--sort=committerdate',
+            '--format=%(refname:short)',
+            'refs/heads',
+            'refs/remotes',
+        ]
+        self._cmd = cmd
+
+    def gather_candidates(self, context):
+        res = self._run_command(self._cmd)
+
+        candidates = [{
+            'word': r,
+            'abbr': r,
+            'action__path': os.path.abspath(r),
+        } for r in res]
+
+        return candidates

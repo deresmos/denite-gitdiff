6633ac2d0ed76e1988ee9c7ddf78dc744e31e9d2
deresmos <deresmos@gmail.com>
2018-08-07 23:31:11 +0900

    Add diff_local actions of gitdiffbranch kind

--- rplugin/python3/denite/kind/gitdiffbranch.py
+++ rplugin/python3/denite/kind/gitdiffbranch.py
@@ -9,20 +9,27 @@ class Kind(Kind):
         self.name = 'gitdiffbranch'
         self.default_action = 'open'
 
-    def _run_gvdiff(self, context, func):
+    def _run_gvdiff(self, context, func, *, local=False):
         for target in context['targets']:
             new_context = copy(context)
             new_context['targets'] = [target]
 
             func(new_context)
-            self.vim.command('Gedit {}:%'.format(target['base_revision']))
+            if not local:
+                self.vim.command('Gedit {}:%'.format(target['base_revision']))
             self.vim.command('Gvdiff {}'.format(target['target_revision']))
 
     def action_openvdiff(self, context):
         self._run_gvdiff(context, self.action_open)
 
+    def action_openvdiff_local(self, context):
+        self._run_gvdiff(context, self.action_open, local=True)
+
     def action_tabvdiff(self, context):
         self._run_gvdiff(context, self.action_tabopen)
+
+    def action_tabvdiff_local(self, context):
+        self._run_gvdiff(context, self.action_tabopen, local=True)
 
     def _run_gedit(self, context, func):
         for target in context['targets']:

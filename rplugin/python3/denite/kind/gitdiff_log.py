6dbaa8919a437b8e407663432db69e8168f8fd3a
deresmos <deresmos@gmail.com>
2019-06-06 11:40:15 +0900

    Change preview window of gitdiff_log

--- rplugin/python3/denite/kind/gitdiff_log.py
+++ rplugin/python3/denite/kind/gitdiff_log.py
@@ -80,35 +80,37 @@ class Kind(Base):
 
     def action_preview(self, context):
         target = context["targets"][0]
-        tmp_op = self.vim.options["splitbelow"]
-        self.vim.options["splitbelow"] = False
 
         if (
-            not context["auto_action"] != "preview"
-            and self.__get_preview_window()
+            context["auto_action"] != "preview"
+            and self._get_preview_window()
             and self._previewed_target == target
         ):
             self.vim.command("pclose!")
             return
-
         prev_id = self.vim.call("win_getid")
-        self.vim.command("pedit! denite_gitdiff_preview")
+
+        self.vim.call("denite#helper#preview_file", context, "diff_log")
         self.vim.command("wincmd P")
+        self.vim.command('execute "normal \<C-w>L"')
+
         self.vim.command("lcd {}".format(target["git_rootpath"]))
         self.vim.command("setlocal filetype=diff")
         self.vim.command("setlocal nobuflisted buftype=nofile bufhidden=unload")
-        self.vim.command("read! git show --stat {}".format(target["base_revision"]))
         self.vim.command(
-            "read! git diff {}...{}".format(
+            "silent read! git show --stat {}".format(target["base_revision"])
+        )
+        self.vim.command(
+            "silent read! git diff {}...{}".format(
                 target["target_revision"], target["base_revision"]
             )
         )
         self.vim.command("1,1delete")
+
         self.vim.call("win_gotoid", prev_id)
         self._previewed_target = target
-        self.vim.options["splitbelow"] = tmp_op
 
-    def __get_preview_window(self):
+    def _get_preview_window(self):
         return next(
             filterfalse(lambda x: not x.options["previewwindow"], self.vim.windows),
             None,
